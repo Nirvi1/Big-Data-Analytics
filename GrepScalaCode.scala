39064762e8731c25e582c9d5ff39051389fba436
Nirvi1 <nirvir2@gmail.com>
2018-07-06 14:36:31 +0530

    Add files via upload

--- GrepScalaCode.scala
+++ GrepScalaCode.scala
@@ -4,3 +4,4 @@ val file3	=	sc.textFile("hdfs://cet4:9000/input/5000-8.txt")
 var ip	=	Seq(file1,file2,file3) 
 var inputFile	=	sc.union(ip) 
 val matchTerm : String = "Leonardo" 
+val numMatches = inputFile.filter(line => line.contains(matchTerm)).counl () numMatches: Long = 627 
