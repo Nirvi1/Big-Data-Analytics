f1db00aa4bd5e7cf64d3e6c67d2afd12a3222f2d
Nirvi1 <nirvir2@gmail.com>
2018-07-06 14:34:23 +0530

    Add files via upload

---
+++ GrepScalaCode.scala
@@ -0,0 +1,6 @@
+val file1	=	sc.textFile("hdfs://cet4:9000/input/pg4300.txt") 
+val file2	=	sc.textFile("hdfs://cet4:9000/input/pg20417.txt") 
+val file3	=	sc.textFile("hdfs://cet4:9000/input/5000-8.txt") 
+var ip	=	Seq(file1,file2,file3) 
+var inputFile	=	sc.union(ip) 
+val matchTerm : String = "Leonardo" 
